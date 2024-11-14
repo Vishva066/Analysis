43ff82d36fe7ae0a71d788ec73598d7edfd6cd17
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2024-11-14 10:34:48 +0530

    Updated client

--- Client_Test/main.go
+++ Client_Test/main.go
@@ -1,161 +1,128 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"time"
-// )
-
-// func main() {
-
-// 	// use this functions to evaluate and submit txns
-// 	// try calling these functions
-// 	startTime := time.Now()
-// 	for i := 0; i <= 50; i++ {
-// 		carKey := fmt.Sprintf("Car1-%d", i)
-
-// 		result := submitTxnFn(
-// 			"manufacturer",
-// 			"autochannel",
-// 			"KBA-Automobile",
-// 			"CarContract",
-// 			"invoke",
-// 			make(map[string][]byte),
-// 			"CreateCar",
-// 			1000,
-// 			carKey,
-// 			"Tata",
-// 			"Harrier",
-// 			"Black",
-// 			"fac01",
-// 			"25/10/2023",
-// 		)
-
-// 		fmt.Println(result)
-// 	}
-
-// 		endTime := time.Now()  // Track end time for TPS calculation
-// 		duration := endTime.Sub(startTime).Seconds()
-// 		fmt.Printf("Total Transactions: 50, TPS: %f\n", float64(50)/duration)
-
-// 	// submitTxnFn("manufacturer", "autochannel", "KBA-Automobile", "CarContract", "test", make(map[string][]byte), "ReadCar",3000,"Car1-9")
-// }
-
 import (
     "fmt"
     "sync"
     "time"
 
-    "github.com/hyperledger/fabric-gateway/pkg/client"   //Fabric Client package
+    "github.com/hyperledger/fabric-gateway/pkg/client"
 )
 
 type TxnCounters struct {
-	success int
-	failure int
-	mu      sync.Mutex //Mutex to lock certain resources while one is updating
+    success int
+    failure int
+    mu      sync.Mutex
 }
 
-func worker(id int, jobs <-chan string, wg *sync.WaitGroup, counters *TxnCounters, organization string, channelName string, chaincodeName string ,contractName string, txnType string, txnName string) {
-    defer wg.Done() //To execute this function after the current function block is excuted
-    // This function tell the waiting list its work is done
+type GatewayConfig struct {
+    gateway  *client.Gateway
+    network  *client.Network
+    contract *client.Contract
+}
 
-    //Client code for fabric
+func initializeGateway(organization, channelName, chaincodeName, contractName string) (*GatewayConfig, error) {
     orgProfile := profile[organization]
-	mspID := orgProfile.MSPID
-	certPath := orgProfile.CertPath
-	keyPath := orgProfile.KeyDirectory
-	tlsCertPath := orgProfile.TLSCertPath
-	gatewayPeer := orgProfile.GatewayPeer
-	peerEndpoint := orgProfile.PeerEndpoint
+    mspID := orgProfile.MSPID
+    certPath := orgProfile.CertPath
+    keyPath := orgProfile.KeyDirectory
+    tlsCertPath := orgProfile.TLSCertPath
+    gatewayPeer := orgProfile.GatewayPeer
+    peerEndpoint := orgProfile.PeerEndpoint
 
     clientConnection := newGrpcConnection(tlsCertPath, gatewayPeer, peerEndpoint)
-
-    Id := newIdentity(certPath, mspID)
-	sign := newSign(keyPath)
+    id := newIdentity(certPath, mspID)
+    sign := newSign(keyPath)
 
     gw, err := client.Connect(
-		Id,
-		client.WithSign(sign),
-		client.WithClientConnection(clientConnection),
-		// Default timeouts for different gRPC calls
-		client.WithEvaluateTimeout(5*time.Second),
-		client.WithEndorseTimeout(30*time.Second),
-		client.WithSubmitTimeout(5*time.Second),
-		client.WithCommitStatusTimeout(1*time.Minute),
-	)
-	if err != nil {
-		panic(err)
-	}
+        id,
+        client.WithSign(sign),
+        client.WithClientConnection(clientConnection),
+        client.WithEvaluateTimeout(5*time.Second),
+        client.WithEndorseTimeout(30*time.Second),
+        client.WithSubmitTimeout(5*time.Second),
+        client.WithCommitStatusTimeout(1*time.Minute),
+    )
+    if err != nil {
+        return nil, fmt.Errorf("failed to connect to gateway: %v", err)
+    }
 
     network := gw.GetNetwork(channelName)
-	contract := network.GetContractWithName(chaincodeName, contractName)
+    contract := network.GetContractWithName(chaincodeName, contractName)
 
-	// fmt.Printf("\n-->Submiting Transaction: %s with ID %s,\n", txnName, args[0])
+    return &GatewayConfig{
+        gateway:  gw,
+        network:  network,
+        contract: contract,
+    }, nil
+}
 
-    //Creating random key values for Car
+func worker(
+    id int,
+    jobs <-chan string,
+    wg *sync.WaitGroup,
+    counters *TxnCounters,
+    gwConfig *GatewayConfig,
+    txnName string,
+) {
+    defer wg.Done()
 
     for carKey := range jobs {
-        // result := submitTxnFn(
-        //     "manufacturer",
-        //     "autochannel",
-        //     "KBA-Automobile",
-        //     "CarContract",
-        //     "invoke",
-        //     make(map[string][]byte),
-        //     "CreateCar",
-        //     carKey,
-        //     "Tata",
-        //     "Harrier",
-        //     "Black",
-        //     "fac01",
-        //     "25/10/2023",
-        // )
-        fmt.Printf("\n-->Submiting Transaction: %s with ID %s,\n", txnName, carKey)
+        fmt.Printf("\n-->Submitting Transaction: %s with ID %s\n", txnName, carKey)
 
-        _, err := contract.SubmitTransaction(txnName, carKey, "Tata", "Harrier", "Black", "fac01", "25/10/2024")
+        _, err := gwConfig.contract.SubmitTransaction(txnName, carKey, "Tata", "Harrier", "Black", "fac01", "25/10/2024")
 
-        //Only one user per time to add the txn failures or success
-
-		counters.mu.Lock()
-		if err != nil {
-			counters.failure++
-		} else {
-			// counters.failure++
+        counters.mu.Lock()
+        if err != nil {
+            counters.failure++
+            fmt.Printf("Transaction failed for %s: %v\n", carKey, err)
+        } else {
             counters.success++
-		}
-        // Unlocking the resources
-
-		counters.mu.Unlock()
+        }
+        counters.mu.Unlock()
     }
 }
 
-
 func main() {
-	var counters TxnCounters //Defining the struct
-    startTime := time.Now() //For TPS Calculation
-    totalTxns := 1000 //Total no of transactions
-    numWorkers := 100   // Total no of workers running parallely
-    jobs := make(chan string, totalTxns)  //  channel to hold transaction jobs (queue)
-    //This can hold the totalTxns no of strings
-    var wg sync.WaitGroup //Set up wait group
+    var counters TxnCounters
+    startTime := time.Now()
+    totalTxns := 5000
+    numWorkers := 100
+    jobs := make(chan string, totalTxns)
 
-    // Start 5 worker goroutines
+    // Initialize gateway once
+    gwConfig, err := initializeGateway(
+        "manufacturer",
+        "autochannel",
+        "KBA-Automobile",
+        "CarContract",
+    )
+    if err != nil {
+        panic(fmt.Sprintf("Failed to initialize gateway: %v", err))
+    }
+    // Ensure gateway is closed after we're done
+    defer gwConfig.gateway.Close()
+
+    var wg sync.WaitGroup
+
+    // Start worker goroutines with shared gateway config
     for w := 1; w <= numWorkers; w++ {
         wg.Add(1)
-        go worker(w, jobs, &wg, &counters,"manufacturer", "autochannel","KBA-Automobile", "CarContract", "invoke", "CreateCar" )
+        go worker(w, jobs, &wg, &counters, gwConfig, "CreateCar")
     }
 
-    // Send 50 transactions as jobs
+    // Send transactions as jobs
     for i := 0; i < totalTxns; i++ {
-        carKey := fmt.Sprintf("Car1-%d", i)
-        jobs <- carKey  // Add job to the jobs channel
+        carKey := fmt.Sprintf("Car2-%d", i)
+        jobs <- carKey
     }
-    close(jobs)  // Close the channel to signal no more jobs
+    close(jobs)
 
-    wg.Wait()  // Wait for all workers to finish
+    wg.Wait()
 
     endTime := time.Now()
     duration := endTime.Sub(startTime).Seconds()
-	fmt.Printf("Successes: %d, Failures: %d\n", counters.success, counters.failure)
-    fmt.Printf("Total Transactions: %d, TPS: %f\n", totalTxns, float64(totalTxns)/duration)
+    fmt.Printf("\nResults:\n")
+    fmt.Printf("Successes: %d, Failures: %d\n", counters.success, counters.failure)
+    fmt.Printf("Total Transactions: %d, Duration: %.2f seconds, TPS: %.2f\n",
+        totalTxns, duration, float64(totalTxns)/duration)
 }
-
