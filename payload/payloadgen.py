85c2cbdce9724a15c6bce81be1f6d7a05cc0ddc4
Vishva066 <vishvaprasad.p.2019.cse@rajalakshmi.edu.in>
2024-11-22 18:16:15 +0530

    payload generator

---
+++ payload/payloadgen.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+# Calculate the number of characters needed for 10KB
+char_count = 51200  # 10KB = 10240 bytes
+
+# Generate the random string
+random_string = generate_random_string(char_count)
+
+# Print the generated string
+print(random_string)
